8df204c283567dbd628b7a78acf1f5d2ea5f6420
DiegoValiño23 <dvalinoc23@fpcoruna.afundacion.org>
2024-10-23 12:46:36 +0200

    Base de datos

---
+++ sprint2sql/mysitedb.sql
@@ -0,0 +1,127 @@
+-- MariaDB dump 10.19  Distrib 10.11.6-MariaDB, for debian-linux-gnu (x86_64)
+--
+-- Host: localhost    Database: mysitedb
+-- ------------------------------------------------------
+-- Server version	10.11.6-MariaDB-0+deb12u1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `tComentarios`
+--
+
+DROP TABLE IF EXISTS `tComentarios`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tComentarios` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `comentario` varchar(2000) DEFAULT NULL,
+  `usuario_id` int(11) DEFAULT NULL,
+  `juego_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `usuario_id` (`usuario_id`),
+  KEY `juego_id` (`juego_id`),
+  CONSTRAINT `tComentarios_ibfk_1` FOREIGN KEY (`usuario_id`) REFERENCES `tUsuarios` (`id`),
+  CONSTRAINT `tComentarios_ibfk_2` FOREIGN KEY (`juego_id`) REFERENCES `tJuegos` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tComentarios`
+--
+
+LOCK TABLES `tComentarios` WRITE;
+/*!40000 ALTER TABLE `tComentarios` DISABLE KEYS */;
+INSERT INTO `tComentarios` VALUES
+(1,'Gran juego',1,1),
+(2,'Realmente difícil',2,2),
+(3,'Muy realista',3,3),
+(4,'Fantástico',4,4),
+(5,'Increíble',5,5);
+/*!40000 ALTER TABLE `tComentarios` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tJuegos`
+--
+
+DROP TABLE IF EXISTS `tJuegos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tJuegos` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(50) DEFAULT NULL,
+  `url_imagen` varchar(200) DEFAULT NULL,
+  `precio` int(11) DEFAULT NULL,
+  `genero` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tJuegos`
+--
+
+LOCK TABLES `tJuegos` WRITE;
+/*!40000 ALTER TABLE `tJuegos` DISABLE KEYS */;
+INSERT INTO `tJuegos` VALUES
+(1,'Elden Ring','https://assets.xboxservices.com/elden-ring.jpg',60,'Fantasia'),
+(2,'Dark Souls 1','https://data.xxlgamer.com/dark-souls-1.jpg',60,'Medieval'),
+(3,'Dark Souls 2','https://www.gamegrin.com/dark-souls-2.jpg',60,'Magia'),
+(4,'Dark Souls 3','https://gaming-cdn.com/dark-souls-3.jpg',60,'Terror'),
+(5,'Sekiro','https://upload.wikimedia.org/sekiro_art.jpg',60,'Samurais');
+/*!40000 ALTER TABLE `tJuegos` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tUsuarios`
+--
+
+DROP TABLE IF EXISTS `tUsuarios`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tUsuarios` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(50) DEFAULT NULL,
+  `apellidos` varchar(100) DEFAULT NULL,
+  `email` varchar(200) DEFAULT NULL,
+  `contraseña` varchar(200) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tUsuarios`
+--
+
+LOCK TABLES `tUsuarios` WRITE;
+/*!40000 ALTER TABLE `tUsuarios` DISABLE KEYS */;
+INSERT INTO `tUsuarios` VALUES
+(1,'Miguel','Vente','miguelvente@gmail.com','12345'),
+(2,'Abel','Roca','abelroca@gmail.com','12345'),
+(3,'Diego','Valiño','diegovalino@gmail.com','12345'),
+(4,'Mario','Garcia','mariogarcia@gmail.com','12345'),
+(5,'Nicolas','Gomez','nicolasgomez@gmail.com','12345');
+/*!40000 ALTER TABLE `tUsuarios` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-10-23 12:46:07
