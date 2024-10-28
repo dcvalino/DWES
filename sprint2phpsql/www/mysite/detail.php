7a4559c3f4a616bb4320108e308c8ee6bf5b9649
DiegoValiño23 <dvalinoc23@fpcoruna.afundacion.org>
2024-10-28 09:29:22 +0100

    Tarea4

--- sprint2phpsql/www/mysite/detail.php
+++ sprint2phpsql/www/mysite/detail.php
@@ -25,5 +25,12 @@ echo '<li>'.$row['comentario'].'</li>';
 mysqli_close($db);
 ?>
 </ul>
+<p>Deja un nuevo comentario:</p>
+<form action="/comment.php" method="post">
+<textarea rows="4" cols="50" name="new_comment"></textarea><br>
+<input type="hidden" name="cancion_id" value="<?php echo $cancion_id; ?>">
+<input type="submit" value="Comentar">
+</form>
+<form>
 </body>
 </html>
