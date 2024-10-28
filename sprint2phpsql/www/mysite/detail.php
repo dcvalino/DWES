35984528561f071a32db25e3fc042217a50474e0
DiegoValiño23 <dvalinoc23@fpcoruna.afundacion.org>
2024-10-28 09:49:37 +0100

    Tarea5 y correcion de la tarea4

--- sprint2phpsql/www/mysite/detail.php
+++ sprint2phpsql/www/mysite/detail.php
@@ -28,7 +28,7 @@ mysqli_close($db);
 <p>Deja un nuevo comentario:</p>
 <form action="/comment.php" method="post">
 <textarea rows="4" cols="50" name="new_comment"></textarea><br>
-<input type="hidden" name="cancion_id" value="<?php echo $cancion_id; ?>">
+<input type="hidden" name="juego_id" value="<?php echo $juego_id; ?>">
 <input type="submit" value="Comentar">
 </form>
 <form>
