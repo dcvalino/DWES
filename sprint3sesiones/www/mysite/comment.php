9691eac9ab5fc5515301835f3092cb25180044d1
DiegoValiño23 <dvalinoc23@fpcoruna.afundacion.org>
2024-11-08 19:50:48 +0100

    sprint3T4

--- sprint3sesiones/www/mysite/comment.php
+++ sprint3sesiones/www/mysite/comment.php
@@ -4,11 +4,16 @@ $db = mysqli_connect('localhost', 'root', '1234', 'mysitedb') or die('Fail');
 <html>
 <body>
 <?php
+session_start();
+$user_id_a_insertar = 'NULL';
+if (!empty($_SESSION['user_id'])) {
+$user_id_a_insertar = $_SESSION['user_id'];
+}
 $juego_id = $_POST['juego_id'];
 $comentario = $_POST['new_comment'];
 $fcomentario = date('Y-m-d');
 $query = "INSERT INTO tComentarios(comentario, juego_id, usuario_id, fcomentario)
- VALUES ('$comentario', $juego_id, NULL, '$fcomentario')";
+ VALUES ('$comentario', $juego_id, $user_id_a_insertar, '$fcomentario')";
 
 mysqli_query($db, $query) or die('Error');
 echo "<p>Nuevo comentario ";
