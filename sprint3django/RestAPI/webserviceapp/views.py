74a90454a6148906e52b5c7b1b19b1c0e03837ed
DiegoValiño23 <dvalinoc23@fpcoruna.afundacion.org>
2024-11-27 14:04:50 +0100

    Ej1 P2

--- sprint3django/RestAPI/webserviceapp/views.py
+++ sprint3django/RestAPI/webserviceapp/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 
 from .models import Tjuegos
+
 # Create your views here.
 def pagina_de_prueba(request):
 	return HttpResponse("<h1>Hola caracola</h1>");
@@ -18,3 +19,19 @@ def devolver_juegos(request):
 		respuesta_final.append(diccionario)
 	return JsonResponse(respuesta_final, safe=False);
 
+def devolver_juegos_por_id(request, id_solicitado):
+	juego = Tjuegos.objects.get(id = id_solicitado)
+	comentarios = juego.tcomentarios_set.all()
+	lista_comentarios = []
+	for fila_comentarios_sql in comentarios:
+			diccionario = {}
+			diccionario['id'] = fila_comentarios_sql.id
+			diccionario['comentario'] = fila_comentarios_sql.comentario
+			lista_comentarios.append(diccionario)
+			resultado = {
+				'id': juego.id,
+				'nombre': juego.nombre,
+				'precio': juego.precio,
+				'comentarios': lista_comentarios
+			}
+			return JsonResponse(resultado, json_dumps_params={'ensure_ascii': False})
