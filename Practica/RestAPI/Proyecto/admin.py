ea076f3f20a490e70e6289d3c1c16bae99d4d36f
Diego Valiño Cabezas <dvalinoc23@fpcoruna.afundacion.org>
2025-01-20 20:28:42 +0100

    Semana1

--- Practica/RestAPI/Proyecto/admin.py
+++ Practica/RestAPI/Proyecto/admin.py
@@ -1,3 +1,8 @@
+# Proyecto/admin.py
 from django.contrib import admin
+from .models import UsuarioPersonalizado, Eventos, Reservas, Comentarios
 
-# Register your models here.
+admin.site.register(UsuarioPersonalizado)
+admin.site.register(Eventos)
+admin.site.register(Reservas)
+admin.site.register(Comentarios)
